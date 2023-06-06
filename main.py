2ac64fd5707434c3e9549b9b21e9ca25475205d2
nRegtLu <1494185212@qq.com>
2023-06-06 19:58:52 +0800

    基本完成主体

--- main.py
+++ main.py
@@ -1,108 +1,128 @@
-def init():
-    # download_GFWlist()
-    with open('./gfw.lst') as f:
-        rules = f.readlines()
-    global adblock_rules
-    adblock_rules = rules
-    with open('./cn.lst') as f:
-        rules = f.readlines()
-    global white_rules
-    white_rules = rules
+import re
+import sqlite3
+import time
 
+class Router(object):
+    def __init__(self):
+        # self.update_GFWlist()
+        with open('./gfw.lst') as f:
+            rules = f.readlines()
+        self.black_rules = rules
+        with open('./cn.lst') as f:
+            rules = f.readlines()
+        self.white_rules = rules
+        self.sql=sqlite3.connect('./rules.sqlite')
 
-def download_GFWlist():
-    import update
-    update.main()
-    del update
+    def update_GFWlist(self):
+        import update
+        update.main()
+        del update
 
+    def in_GFW(self, url):  # True or False
+        for rule in self.black_rules:
+            res = re.findall(rule.replace('\n', ''), url)
+            if res == [] or len(res) > 1 or res[0] != url:
+                continue
+            else:
+                return True
+        return False
 
-def in_GFW(url):  # True or False
-    import re
-    for rule in adblock_rules:
-        res = re.findall(rule.replace('\n', ''), url)
-        if res == [] or len(res) > 1 or res[0] != url:
-            continue
+    def in_whitelist(self, url):  # True or False
+        for rule in self.white_rules:
+            res = re.findall(rule.replace('\n', ''), url)
+            if len(res) == 1:
+                return True
+            else:
+                continue
+        return False
+
+    def custom(self,url):
+        cursor=self.sql.cursor()
+        cursor.execute('SELECT * FROM RULES WHERE PROXY = TRUE')
+        rules=cursor.fetchall()
+        for rule in rules:
+            res = re.findall(rule, url)
+            if res == [] or len(res) > 1 or res[0] != url:
+                continue
+            else:
+                cursor.close()
+                return 'Proxy'
+        cursor.execute('SELECT * FROM RULES WHERE PROXY = FALSE')
+        rules=cursor.fetchall()
+        for rule in rules:
+            res = re.findall(rule, url)
+            if res == [] or len(res) > 1 or res[0] != url:
+                continue
+            else:
+                cursor.close()
+                return 'Direct'
+        cursor.close()
+        return None
+        
+    def test(self, url):  # 'Direct' or 'Proxy' or 'Not Found'
+        proxy = {
+            'http': 'http://127.0.0.1:7890',
+            'https': 'http://127.0.0.1:7890'
+        }
+        url = r'https://'+url+'/robots.txt'
+        import requests
+        import time
+        try:
+            start_time = time.time()
+            response = requests.get(url, timeout=3)
+            end_time = time.time()
+            direct_time = end_time - start_time
+        except:
+            direct_time = 100
+        try:
+            start_time = time.time()
+            response = requests.get(url, proxies=proxy, timeout=3)
+            end_time = time.time()
+            proxy_time = end_time - start_time
+        except:
+            proxy_time = 99
+        del requests
+        del time
+        if direct_time < proxy_time:
+            self.add(url,'Direct')
+            return False
         else:
+            self.add(url,'Proxy')
             return True
-    return False
 
 
-def in_direct(url):
-    pass
-
-
-def in_proxy(url):
-    pass
-
-def to_direct(url):
-    pass
-
-
-def to_proxy(url):
-    pass
-
-
-def in_whitelist(url):  # True or False
-    import re
-    for rule in white_rules:
-        res = re.findall(rule.replace('\n', ''), url)
-        if len(res) == 1:
-            return True
+    def add(self,url,route):
+        if route=='Proxy':
+            arg='TRUE'
+        elif route=='Direct':
+            arg='FALSE'
         else:
-            continue
-    return False
+            return
+        self.sql.execute("INSERT INTO RULES VALUES('$1',$2,$3)"%(url,int(time.time()),arg))
 
 
-def test_robot(url):  # 'Direct' or 'Proxy' or 'Not Found'
-    proxy = {
-        'http': 'http://127.0.0.1:7890',
-        'https': 'http://127.0.0.1:7890'
-    }
-    url = r'https://'+url+'/robots.txt'
-    import requests
-    import time
-    try:
-        start_time = time.time()
-        response = requests.get(url, timeout=3)
-        end_time = time.time()
-        direct_time = end_time - start_time
-    except:
-        direct_time = 100
-    try:
-        start_time = time.time()
-        response = requests.get(url, proxies=proxy, timeout=3)
-        end_time = time.time()
-        proxy_time = end_time - start_time
-    except:
-        proxy_time = 99
-    del requests
-    del time
-    if direct_time < proxy_time:
-        to_direct(url)
-        return 'Direct'
-    else:
-        to_proxy(url)
-        return 'Proxy'
+    def route(self, url):
+        if self.in_whitelist(url) == True:
+            return ('Direct', 'GFW')
+        if self.in_GFW(url) == True:
+            return ('Proxy', 'GFW')
+
+        custom_ans = self.custom(url)
+        if custom_ans == 'Direct':
+            return ('Direct', 'Custom')
+        elif custom_ans == 'Proxy':
+            return ('Proxy', 'Custom')
+
+        test_ans = self.test(url)
+        if test_ans:
+            return ('Proxy', 'Test')
+        else:
+            return ('Direct', 'Test')
+    
+    def refresh_custom(self):
+        self.sql.execute("DELETE FROM RULES WHERE strftime('%s','now') - TIME > 864000")
 
 
-def detect(url):
-    if in_whitelist(url) == True:
-        return ('Whitelist', 'Direct')
-    if in_GFW(url) == True:
-        return ('GFW', 'Proxy')
-    if in_direct(url) == True:
-        return ('Custom', 'Direct')
-    if in_proxy(url) == True:
-        return ('Custom', 'Proxy')
-    robot_ans = test_robot(url)
-    if robot_ans == 'Proxy':
-        return ('Robots', 'Proxy')
-    elif robot_ans == 'Direct':
-        return ('Robots', 'Direct')
-    return ('Final', 'Proxy')
 
-
-init()
-print(detect('www.google.com'))
-print(detect('www.baidu.com'))
-print(detect('cdda.fun'))
+router = Router()
+print(router.route('www.google.com'))
